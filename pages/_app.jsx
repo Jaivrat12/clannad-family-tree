2934bb8061f382cf1e2f3aa5545854bdd705d217
Jaivrat Das <jaivratdas123456@gmail.com>
2022-12-23 10:48:25 +0530

    Updates to Head sections

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import { CssVarsProvider } from '@mui/joy/styles';
 import CssBaseline from '@mui/joy/CssBaseline';
 import clannadTheme from '../utils/theme';
@@ -10,10 +11,18 @@ import '@fontsource/public-sans';
 
 export default function App({ Component, pageProps }) {
 
+	const title = (Component.title || '') && Component.title + ' | ';
+
 	return (
 
 		<CssVarsProvider theme={ clannadTheme }>
 			<CssBaseline />
+			<Head>
+				<title>
+					{ `${ title }Clannad - Family Tree Maker` }
+				</title>
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+			</Head>
 			<Component {...pageProps} />
 		</CssVarsProvider>
 	);
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,19 +1,12 @@
-import Head from 'next/head';
 import Typography from '@mui/joy/Typography';
 
 
+Home.title = 'Home';
 export default function Home() {
 
 	return (
 
 		<>
-			<Head>
-				<title>Home | Clannad - Family Tree Maker</title>
-				<meta name="description" content="Clannad - Family Tree Maker" />
-				<meta name="viewport" content="width=device-width, initial-scale=1" />
-				<link rel="icon" href="/favicon.ico" />
-			</Head>
-
 			<Typography
 				level="h2"
 				textAlign="center"
